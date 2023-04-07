8b4d7cdc81e53ae5011fb833323322ec86b7a0d1
Luis020-hub <guhmonteiro10@hotmail.com>
2023-04-07 18:19:19 -0300

    07/04/2023
    - Usando Kotlin para somar(Android Studio).
    - Criando o método para calcular no  flutter(Visual Studio Code).

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +33,20 @@ class _MyHomePageState extends State<MyHomePage> {
   int _a = 0;
   int _b = 0;
 
-  void _calcSum() {
+  Future<void> _calcSum() async {
+    const channel = MethodChannel('google.com.br'); // Exemplo genérico
+
+    try {
+      final sum = await channel.invokeMethod('calcSum', {"a": _a, "b": _b});
+      setState(() {
+        _sum = sum;
+      });
+    } on PlatformException {
+      setState(() {
+        _sum = 0;
+      });
+    }
+
     setState(() {
       _sum = _a + _b;
     });
